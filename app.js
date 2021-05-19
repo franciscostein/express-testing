aabf8085e4925eebaea88ff63bbc8644593fa450
Leandro Costa <leandro.franc2008@hotmail.com>
2021-05-18 21:41:07 -0300

    lesson 27

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const todoRoutes = require('./routes/todo.routes');
+const mongodb = require('./mongodb/mongodb.connect');
 
 const app = express();
+
+mongodb.connect();
 
 app.use(express.json());
 
@@ -11,6 +14,6 @@ app.get('/', (req, res) => {
     res.json('Hello fucking world!');
 });
 
-app.listen(3000, () => console.log('server is up & running!'));
+// app.listen(3000, () => console.log('server is up & running!'));
 
 module.exports = app;
---
+++ mongodb/mongodb.connect.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+async function connect() {
+    try {
+        await mongoose.connect('mongodb+srv://root:hMliWR2NzpEXuv8R@cluster.fmt0m.mongodb.net/express-testing?retryWrites=true&w=majority');
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+module.exports = { connect }
